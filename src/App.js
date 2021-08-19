64c77029650a8f592fbc1e8546aa11be14a33f11
keshav singh <keshav.singh@wobot.ai>
2021-08-19 03:21:20 -0700

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+//import Header from "components/Header";
+//import AppConfig from "App.config";
+//import ExternalInfo from "components/ExternalInfo";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import { Button, Modal } from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [otp, setOtp] = useState(new Array(6).fill(""));
+    const [modalShow, setModalShow] = useState(false);
+    const show = () => {
+        setModalShow(!modalShow);
+    }
+
+
+    const handleChange = (element, index) => {
+        if (isNaN(element.value)) return false;
+
+        setOtp([...otp.map((d, idx) => (idx === index ? element.value : d))]);
+
+        //Focus next input
+        if (element.nextSibling) {
+            element.nextSibling.focus();
+        }
+        
+    };
+
+    return (
+        <>
+          {/*  <Header title="Building OTP box using Hooks" />
+
+    <ExternalInfo page="otpBox" />*/}
+    <button type="button" class="btn btn-primary" onClick = {show} data-toggle="modal" data-target="#exampleModal">
+  PHONE VERIFICATION
+</button>
+
+            <div>
+                <div>
+                  
+                    <Modal
+                
+                show={modalShow}
+                onHide={() => {
+                    setModalShow(false);
+                }}
+            >
+                <Modal.Header />
+                <Modal.Body>
+                    <div className="modal-popup">
+                        <p className="fs-20 primary-color fw-500">
+                            Phone Verification
+                        </p>
+                    </div>
+                </Modal.Body>
+                <Modal.Footer >
+                    {/*<div className="align-right">*/}
+                  
+                    {/*</div>*/}
+                    <div className="form-row">
+                    {otp.map((data, index) => {
+                        return (
+                            
+                            <input
+                                style={{display:"inline"}}
+                
+                              //id="search-form"
+                                type="text"
+                                name="otp"
+                                maxLength="1"
+                                key={index}
+                                value={data}
+                                onChange={e => handleChange(e.target, index)}
+                                onFocus={e => e.target.select()}
+                                //onCopy = {e => e.target.value}
+                                
+                            />
+                    
+                        );
+                    })}
+                    </div>
+
+                    <p>OTP Entered - {otp.join("")}</p>
+                    <p>
+                        <button
+                            className="btn btn-secondary mr-2"
+                            onClick={e => setOtp([...otp.map(v => "")])}
+                        >
+                            Clear
+                        </button>
+                       
+                    </p>
+
+
+
+
+                </Modal.Footer>
+            </Modal>
+            
+
+
+                    
+                </div>
+            </div>
+        </>
+    );
+};
 
 export default App;
